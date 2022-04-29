14a4a10f66eadb331afd3cef07ab9a2d0b73eb74
Faizan Ejaz <abc@example.com>
2022-04-29 18:57:18 +0500

    Switch Adding

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -15,22 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView im = findViewById(R.id.im);
-        Switch sw = findViewById(R.id.sw);
-
-
-        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    im.setImageResource(R.drawable.yaa);
-                } else {
-
-                    im.setImageResource(R.drawable.before);
-                }
-            }
-
-
-        });
 
     }
 
