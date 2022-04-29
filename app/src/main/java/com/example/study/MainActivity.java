309fed2ed32c2953664dc296b9dde83fc47a8ae4
Faizan Ejaz <abc@example.com>
2022-04-29 18:52:54 +0500

    SCREENSHOTS ADDED

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.study;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView im = findViewById(R.id.im);
+        Switch sw = findViewById(R.id.sw);
+
+
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    im.setImageResource(R.drawable.yaa);
+                } else {
+
+                    im.setImageResource(R.drawable.before);
+                }
+            }
+
+
+        });
+
     }
+
 }
