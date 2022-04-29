cf1eaea8437194a0bb817ab839ed00280030dd4e
Faizan Ejaz <abc@example.com>
2022-04-29 18:59:02 +0500

    Final Version

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -17,8 +17,19 @@ public class MainActivity extends AppCompatActivity {
         ImageView im = findViewById(R.id.im);
         Switch sw = findViewById(R.id.sw);
 
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    im.setImageResource(R.drawable.yaa);
+                } else {
 
-        
+                    im.setImageResource(R.drawable.before);
+                }
+            }
+
+
+        });
+
 
 
     }
