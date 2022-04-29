3145142bd46c3c0b48173e18d0af0760ed0b4700
Faizan Ejaz <abc@example.com>
2022-04-29 18:58:11 +0500

    Wrong Screenshots adeed

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -14,6 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView im = findViewById(R.id.im);
+        Switch sw = findViewById(R.id.sw);
+
+
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    im.setImageResource(R.drawable.yaa);
+                } else {
+
+                    im.setImageResource(R.drawable.before);
+                }
+            }
+
+
+        });
 
 
     }
